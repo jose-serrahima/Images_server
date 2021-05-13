1af64674d24dd100f9b23205cc3be958dfdbb807
jose <jose@canijo>
2021-05-13 16:56:18 +0200

    First aproach to json parsing

--- scripts/parse_json.py
+++ scripts/parse_json.py
@@ -1,12 +1,35 @@
 import json
+import os
 
-f = open('file.json',)
-data = json.load(f)
+# Reads all files on a proyect, get selected packages from them 
+# and creates end files ready for start execution
 
-for i in data['bloques']:
-	g = open(i['block']+".txt", "w")
-	for j in i['programs']:
-		g.write(j+"\n")
-	g.close()
+# Change directory to read from here
+directory = r'../'
 
-f.close()
+for filename in os.listdir(directory):
+	if filename.endswith(".json"):
+		with open(os.path.join(directory+filename),'r') as file:
+			my_json=json.load(file)
+			g = open(filename+".list.chroot", "w")
+			for k in my_json["prog"]:
+				if k["selected"]:
+					g.write(k["name"]+"\n")
+			g.close()
+		file.close()
+		continue
+	else:
+		continue
+
+# Delete empty files
+
+# TODO: delete extension file for destiny files
+
+directory2 = './'
+
+for filename in os.listdir(directory2):
+	if os.stat(filename).st_size == 0:
+		os.remove(filename) 
+		continue
+	else:
+		continue
