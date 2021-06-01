d1dfac1ff5fbe950076d343072629c830f3c73db
JSerrahima <jose.serrahima@gmail.com>
2021-06-01 14:25:02 +0200

    Update install_packages.sh

--- server-config/install_packages.sh
+++ server-config/install_packages.sh
@@ -2,8 +2,9 @@
 
 echo "RUN WITH SUDO"
 
-apt install curl wget net-tools make sudo live-build -y
+apt install curl wget net-tools make sudo live-build -y;
 # If you have another user change it here.
-usermod -aG sudo user
+usermod -aG sudo user;
+echo "user  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers;
 
 echo "Installation finished"
