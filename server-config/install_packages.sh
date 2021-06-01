1644037d0d7d5aa2e4080e8ab0a29facdceb3fc0
JSerrahima <jose.serrahima@gmail.com>
2021-06-01 14:21:23 +0200

    Update install_packages.sh

--- server-config/install_packages.sh
+++ server-config/install_packages.sh
@@ -2,6 +2,8 @@
 
 echo "RUN WITH SUDO"
 
-apt install curl wget net-tools -y
+apt install curl wget net-tools make sudo live-build -y
+# If you have another user change it here.
+usermod -aG sudo user
 
 echo "Installation finished"
