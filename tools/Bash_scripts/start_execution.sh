41218417a0c157edcd100a3ba0e8ba1ea1015e43
jserrahima <jserrahima@gmail.com>
2021-06-03 18:47:38 +0200

    Added basic configuration to image creation

--- tools/Bash_scripts/start_execution.sh
+++ tools/Bash_scripts/start_execution.sh
@@ -16,25 +16,30 @@ tasks='../debian-live-config/config/package-lists/tasks.json.list.chroot'
 
 if [ -f "$tasks" ]; then
 	echo "*** Configuring desktop ***"
-
+	cp ../Basic_config/desktop.list.chrot ../debian-live-config/config/package-lists/
 	if grep -q kde $tasks; then
   		echo '#!/bin/bash
 echo "I: running $0"
 update-alternatives --set x-session-manager /usr/bin/startkde' > '../debian-live-config/config/hooks/normal/0425-update-session-manager-alternative.hook.chroot'
-	elif grep -q gnome $tasks; then
-		echo '#!/bin/bash
-echo "I: running $0"
-update-alternatives --set x-session-manager /usr/bin/gnome-session' > '../debian-live-config/config/hooks/normal/0425-update-session-manager-alternative.hook.chroot'
 	elif grep -q xfce $tasks; then
 		echo '#!/bin/bash
 echo "I: running $0"
 update-alternatives --set x-session-manager /usr/bin/xfce4-session' > '../debian-live-config/config/hooks/normal/0425-update-session-manager-alternative.hook.chroot'
+		cp ../Basic_config/xfce.utilities.list.chrot ../debian-live-config/config/package-lists/
 	else
 		echo '#!/bin/bash
 echo "I: running $0"
 update-alternatives --set x-session-manager /usr/bin/gnome-session' > '../debian-live-config/config/hooks/normal/0425-update-session-manager-alternative.hook.chroot'
+		cp ../Basic_config/gnome.utilities.list.chrot ../debian-live-config/config/package-lists/
 	fi
 fi 
+
+echo '*** Coping basic configuration ***'
+cp ../Basic_config/00-important.list.chroot ../debian-live-config/config/package-lists/
+cp ../Basic_config/00-required.list.chroot ../debian-live-config/config/package-lists/
+cp ../Basic_config/basic.list.chroot ../debian-live-config/config/package-lists/
+cp ../Basic_config/installet.list.binary ../debian-live-config/config/package-lists/
+echo '*** End basic configuration ***'
 
 #echo '*** Ensuring system has all needed packages ***'
 #dlc='../debian-live-config/'
